8b3d60f70f6cb41bf99acdb2ebc77f7d2e55e28d
joshualondono <joshualondono@outlook.com>
2020-06-15 14:53:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,43 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
+function isItLong(str){
+  if (str.length >= 20) {
+   return 'Thats a long string!'
+  }
+}
+
+function isItMedium(str){
+  if (str.length >= 10 && str.length <= 20) {
+    return 'Thats a regular sized string'
+  }
+}
+
+function isItShort(str){
+  if (str.length <= 10) {
+    return 'Thats a small string'
+  }
+  else {
+    return 'Thats not a small string!'
+  }
+}
+
+function howLongIsMyString(str){
+  
+}
+
+function instructorHeight(height){
+  if (height === 'Colin'){
+    return 62
+  }
+  else if (height === 'Mesuara'){
+    return 69
+  }
+  else {
+    return 'I dont know that instructor!'
+  }
+}
+
 
 
 
